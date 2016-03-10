18896df0c2355b2b4d8efac8599b0533856e92d3
rixx <rixx@cutebit.de>
2016-03-10 10:49:06 +0100

    finished restructuring

--- axsemantics/__init__.py
+++ axsemantics/__init__.py
@@ -1,15 +1,13 @@
 # AX-Semantics Python bindings
 # API docs at https://apidocs.ax-semantics.com
-from axsemantics.axresource import (
-    ContentProject,
-    Thing,
-    login,
-)
-
 import axsemantics.constants
-
 from axsemantics.errors import (
     APIConnectionError,
     APIError,
     AuthenticationError,
 )
+from axsemantics.resources import (
+    ContentProject,
+    Thing,
+)
+from axsemantics.utils import login
