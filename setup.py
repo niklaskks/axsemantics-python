aa7b50a36464325030f92de83e9e17cf434ccc46
rixx <rixx@cutebit.de>
2016-03-15 11:58:47 +0100

    added pypi integration

---
+++ setup.py
@@ -0,0 +1,38 @@
+from codecs import open
+from os import path
+from setuptools import setup
+
+setup(
+    name = 'axsemantics',
+    version = '0.0.1',
+
+    description = 'AXSemantics API client',
+
+    url = 'https://github.com/axsemantics/axsemantics-python',
+    download_url = 'https://github.com/axsemantics/axsemantics-python/tarball/0.1',
+
+    author = 'Tobias Kunze',
+    author_email = 'tobias.kunze@ax-semantics.com',
+
+    license = 'MIT',
+
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages = ['axsemantics'],
+    scripts = ['bin/excel_upload.py'],
+
+    install_requires = [
+        'requests==2.9.1',
+    ],
+
+    extras_require = {
+        'examples': ['pandas==0.17.1'],
+    },
+)
